3a222d321458dae311211766ce318105275449d3
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-03-22 09:19:57 +0100

    mas pruebas

--- clases/inmortal.py
+++ clases/inmortal.py
@@ -1,22 +1,24 @@
-class Yin: pass 
-class Yang: 
+class Yin: pass
+class Yang:
     def __del__(self): 
         print("Yang destruido") 
  
 yin = Yin() 
 yang = Yang() 
-yin.yang = yang
+yin.o = yang
  
 print(yang)
 #>>> <__main__.Yang object at 0x1011da828> 
-print(yang is yin.yang) 
+print(yang is yin.o) 
 #>>> True
 del(yang)
 print("?")
 print("codigo destruido")
 n=0
-while n<10:
+while n<5:
     print(1)
     n+=1
 #>>> ?
 #Yang destruido
+
+#El mensaje aparece despues de la interrogación por culpa de la variable yin.yang .
