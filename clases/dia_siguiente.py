22c337a3ad1883e5f25140a7459718d8ab9d29d0
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-03-22 19:42:46 +0100

    Update dia_siguiente.py

--- clases/dia_siguiente.py
+++ clases/dia_siguiente.py
@@ -2,12 +2,12 @@ class empresa:
     def __init__(self, nombre):
         self.nombre = nombre
 
-    class ciudad:
-        def __init__(self, nombre):
-            self.nombre = nombre
+class ciudad:
+    def __init__(self, nombre):
+        self.nombre = nombre
 
-    Nueva_York = ciudad("Nueva_York")
-    Los_Angeles = ciudad("Los_Angeles")
+Nueva_York = ciudad("Nueva_York")
+Los_Angeles = ciudad("Los_Angeles")
 
 class empleado
 class edificio
