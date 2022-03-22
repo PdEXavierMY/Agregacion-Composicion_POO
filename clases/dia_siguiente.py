328e0cb6af1ddb095d8396d71dbcd202ba0a89a8
Xavitheforce <javier.miguelez.yague@gmail.com>
2022-03-22 19:42:24 +0100

    Create dia_siguiente.py

---
+++ clases/dia_siguiente.py
@@ -0,0 +1,13 @@
+class empresa:
+    def __init__(self, nombre):
+        self.nombre = nombre
+
+    class ciudad:
+        def __init__(self, nombre):
+            self.nombre = nombre
+
+    Nueva_York = ciudad("Nueva_York")
+    Los_Angeles = ciudad("Los_Angeles")
+
+class empleado
+class edificio
